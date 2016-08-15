6d64dcac0dbbd025bf9d309bb4bc508da2720d19
test <test@TEST-WIN8>
2016-08-15 13:31:45 +0900

    プロジェクト ファイルを追加します。

---
+++ NotifiReboot/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NotifiReboot
+{
+    public partial class Form1 : Form
+    {
+        //変数の定義
+        int timerInterval;      //タイマー間隔（ミリ秒）
+        int notifiSpanH, notifiSpanM, notifiSpanS;
+        TimeSpan notifiSpan;
+        int notifiTimes;
+        string notifiMsg;
+        DateTime startTime = DateTime.Now;  //起動時刻
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   //アプリケーション開始時
+
+            //設定読み込み
+            FileStream fs = new FileStream(@".\Settings.xml", FileMode.Open);
+            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(Settings));
+            Settings settings = (Settings)serializer.Deserialize(fs);
+            //設定を変数に代入
+            timerInterval = settings.TimerInterval;
+            notifiSpanH = settings.NotifiSpanH;
+            notifiSpanM = settings.NotifiSpanM;
+            notifiSpanS = settings.NotifiSpanS;
+            notifiSpan = new TimeSpan(notifiSpanH, notifiSpanM, notifiSpanS);      //メッセージ通知時間
+            notifiTimes = settings.NotifiTimes;
+            notifiMsg = settings.NotifiMsg;
+
+            Timer timer = new Timer();
+            timer.Tick += new EventHandler(CheckTime);
+            timer.Interval = timerInterval;
+            timer.Enabled = true;   //タイマー開始
+        }
+
+        private void CheckTime(object sender, EventArgs e)
+        {   //10分ごとに作動する動作
+            DateTime nowTime = DateTime.Now;
+            TimeSpan runningSpan = nowTime - startTime;
+
+            if (runningSpan >= notifiSpan)
+            {   //起動から特定の時間が経過していたら
+                MessageBox.Show(notifiMsg,
+                    "起動時間超過",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+
+
+
+}
---
+++ NotifiReboot/Settings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotifiReboot
+{
+    [System.Xml.Serialization.XmlRoot("settings")]
+    public class Settings
+    {
+        /// <summary>
+        /// タイマー間隔
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("timerInterval")]
+        public int TimerInterval { get; set; }
+        /// <summary>
+        /// 通知時間（時間）
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("notifiSpanH")]
+        public int NotifiSpanH { get; set; }
+        /// <summary>
+        /// 通知時間（分）
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("notifiSpanM")]
+        public int NotifiSpanM { get; set; }
+        /// <summary>
+        /// 通知時間（秒）
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("notifiSpanS")]
+        public int NotifiSpanS { get; set; }
+        /// <summary>
+        /// 再通知回数
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("notifiTimes")]
+        public int NotifiTimes { get; set; }
+        /// <summary>
+        /// 通知メッセージ
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("notifiMsg")]
+        public String NotifiMsg { get; set; }
+    }
+}
